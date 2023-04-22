6da20e35c278c05296b44a32a04fd485385b814f
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-04-22 13:12:55 +0200

    Transition Phases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,16 @@ import { Component } from '@angular/core';
       })),
       transition('normal => highlighted', animate(300)), 
       transition('highlighted => normal', animate(800)),
-      transition('shrunken <=> *', animate(500))
+      transition('shrunken <=> *', [
+        style({ 'background-color': 'orange', borderRadius: '0' }),
+        animate(
+          1000,
+          style({
+            borderRadius: '50px'
+          })
+        ),
+        animate(500)
+])
     ])
   ]
 })
